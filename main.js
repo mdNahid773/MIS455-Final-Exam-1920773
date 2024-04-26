054a7da76b03c10636b1d42a3f85987fffc977f5
mdNahid773 <128293518+mdNahid773@users.noreply.github.com>
2024-04-26 22:51:40 +0600

    fifth commit

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ function connect() {
         .then(res => res.json())
         .then(data => display(data.meals));
 }
+
+function display(items) {
+    var container = document.getElementById("container");
+    container.innerHTML = ""; 
+
+    if (!items) {
+        container.innerHTML = "<p>No results found</p>";
+        return;
+    }
+
+    var limit = Math.min(items.length, 5); 
+
+    for (var i = 0; i < limit; i++) {
+        var newDiv = createMealCard(items[i]);
+        container.appendChild(newDiv);
+    }
+
+    if (items.length > 5) {
+    
+        var showAllButton = document.createElement("button");
+        showAllButton.textContent = "SHOW ALL";
+        showAllButton.classList.add("showAllButton");
+        showAllButton.addEventListener("click", function() {
+            for (var i = 5; i < items.length; i++) {
+                var newDiv = createMealCard(items[i]);
+                container.appendChild(newDiv);
+            }
+            showAllButton.style.display = "none"; 
+        });
+        var buttonContainer = document.createElement("div");
+        buttonContainer.style.textAlign = "center";
+        buttonContainer.appendChild(showAllButton);
+        container.appendChild(buttonContainer);
+    }
+    
+    
+
+    document.getElementById("mainImage").style.visibility = "hidden";
+}
